c4bbd271ce37ecaaaa2e8a3134229199ef44521d
VickyDarmana <134504626+VickyDarmana@users.noreply.github.com>
2025-02-24 21:43:36 +0700

    set up database

--- UTS/app.js
+++ UTS/app.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "testingdb",
+    database: "utsproject",
     });
 
 db.connect((err) => {
