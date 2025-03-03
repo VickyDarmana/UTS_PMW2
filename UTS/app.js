e76e79478153e51cd51b2fb25ea360b34c3e20f0
VickyDarmana <134504626+VickyDarmana@users.noreply.github.com>
2025-03-03 22:17:01 +0700

    update port

--- UTS/app.js
+++ UTS/app.js
@@ -58,5 +58,5 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
